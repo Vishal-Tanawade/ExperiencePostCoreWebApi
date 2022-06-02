94343b78ccc272bde619ef66146030f8e6e1c3ff
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-02 10:02:24 +0530

    ProfilePicture changed - migration added

---
+++ ExperiencePostCoreWebApi/Migrations/20220602042744_EmployeeTableUpdate.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExperiencePostCoreWebApi.Migrations
+{
+    public partial class EmployeeTableUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmpID",
+                keyValue: 1,
+                column: "ProfilePicture",
+                value: "noImage.png");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmpID",
+                keyValue: 2,
+                column: "ProfilePicture",
+                value: "noImage.png");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmpID",
+                keyValue: 3,
+                column: "ProfilePicture",
+                value: "noImage.png");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmpID",
+                keyValue: 1,
+                column: "ProfilePicture",
+                value: "no-image");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmpID",
+                keyValue: 2,
+                column: "ProfilePicture",
+                value: "no-image");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmpID",
+                keyValue: 3,
+                column: "ProfilePicture",
+                value: "no-image");
+        }
+    }
+}
--- ExperiencePostCoreWebApi/Models/ModelBuilderExtensions.cs
+++ ExperiencePostCoreWebApi/Models/ModelBuilderExtensions.cs
@@ -11,9 +11,9 @@ namespace ExperiencePostCoreWebApi.Models
         public static void Seed(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Employee>().HasData(
-                 new Employee { EmpID = 1, FirstName = "Aaron", LastName = "Hawkins", Password = "arron@123", CellNumber = "(660) 663-4518", Email = "aron.hawkins@aol.com",ProfilePicture="no-image" },
-                 new Employee { EmpID = 2, FirstName = "Hedy", LastName = "Greene", Password = "hedy@123", CellNumber = "(608) 265-2215", Email = "hedy.greene@aol.com", ProfilePicture = "no-image" },
-                 new Employee { EmpID = 3, FirstName = "Melvin", LastName = "Porter", Password = "melvin@123", CellNumber = "(959) 119-8364", Email = "melvin.porter@aol.com", ProfilePicture = "no-image" }
+                 new Employee { EmpID = 1, FirstName = "Aaron", LastName = "Hawkins", Password = "arron@123", CellNumber = "(660) 663-4518", Email = "aron.hawkins@aol.com",ProfilePicture= "noImage.png" },
+                 new Employee { EmpID = 2, FirstName = "Hedy", LastName = "Greene", Password = "hedy@123", CellNumber = "(608) 265-2215", Email = "hedy.greene@aol.com", ProfilePicture = "noImage.png" },
+                 new Employee { EmpID = 3, FirstName = "Melvin", LastName = "Porter", Password = "melvin@123", CellNumber = "(959) 119-8364", Email = "melvin.porter@aol.com", ProfilePicture = "noImage.png" }
              );
             modelBuilder.Entity<Skill>().HasData(
             new Skill { SkillId = 1, EmpID = 1, SkillName = "Microsoft Office Suite", Role = "Business Analyst", ExperienceInYears = 2 },
